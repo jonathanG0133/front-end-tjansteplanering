49f4873de58dc8b5c3d9ad352a04650f484b6a57
kaynan9 <118274632+kaynan9@users.noreply.github.com>
2023-12-04 13:09:08 +0100

    Färgskala + labels + streck för perioder

--- react-hig/src/Heatmap.js
+++ react-hig/src/Heatmap.js
@@ -28,11 +28,49 @@ const Heatmap = () => {
     .attr("height", height)
     .attr("viewBox", [0, 0, width, height]);
 
-  // Define the color scale for the heatmap
+  // Create a separate SVG for the color scale
+  const colorScaleSvg = d3.create("svg")
+    .attr("width", 50) // Increased width to accommodate labels
+    .attr("height", height)
+    .attr("viewBox", [0, 0, 50, height]);
+
+  // Create a linear color scale for the color scale
   const colorScale = d3
-    .scaleSequential()
-    .domain([0, 1])
-    .interpolator(d3.interpolateViridis);
+    .scaleLinear()
+    .domain([1, 1 / 2, 0])
+    .range(["blue", "green", "red"]);
+
+  // Create rectangles for the color scale
+  colorScaleSvg
+    .selectAll("rect")
+    .data(d3.range(height))
+    .join("rect")
+    .attr("width", 20) // Width of the color scale
+    .attr("height", 1)
+    .attr("x", 0)
+    .attr("y", (d) => d)
+    .attr("fill", (d) => colorScale(d / height));
+
+  // Add labels for the color scale
+  const colorLabels = ["< 100%", "100%", "> 100%"];
+
+  colorScaleSvg
+    .selectAll("text")
+    .data(colorLabels)
+    .join("text")
+    .attr("x", 30) // Positionen för texten (justera detta värde)
+    .attr("y", (d, i) => (i * height) / (colorLabels.length - 1) + 6) // Positionen för texten (justera detta värde)
+    .attr("dy", "0.35em")
+    .style("font-size", "12px")
+    .style("text-anchor", "start")
+    .text((d) => d);
+
+  // Define the color scale for the heatmap
+  const customColorScale = d3
+    .scaleLinear()
+    .domain([1, 1 / 2, 0])
+    .range(["red", "green", "blue"])
+    .interpolate(d3.interpolateRgb);
 
   // Create scales for x and y axes
   const xScale = d3
@@ -56,7 +94,22 @@ const Heatmap = () => {
     .attr("y", (d) => yScale(d.teacher))
     .attr("width", xScale.bandwidth())
     .attr("height", yScale.bandwidth())
-    .attr("fill", (d) => colorScale(d.value));
+    .attr("fill", (d) => customColorScale(d.value));
+
+  // Create vertical lines to represent periods
+  const periodLines = [13, 26, 39, 52]; // Divide 52 weeks into 4 periods
+  svg
+    .selectAll(".period-line")
+    .data(periodLines)
+    .enter()
+    .append("line")
+    .attr("class", "period-line")
+    .attr("x1", (d) => xScale(d - 0.5))
+    .attr("x2", (d) => xScale(d - 0.5))
+    .attr("y1", margin.top)
+    .attr("y2", height - margin.bottom)
+    .style("stroke", "black")
+    .style("stroke-dasharray", "3,3");
 
   // Create x-axis with only week numbers
   svg
@@ -95,15 +148,17 @@ const Heatmap = () => {
   // Create a reference to a div element
   const divRef = useRef(null);
 
-  // Use the useEffect hook to append the SVG element to the div element
+  // Use the useEffect hook to append the SVG elements to the div element
   useEffect(() => {
     if (divRef.current) {
       divRef.current.appendChild(svg.node());
+      divRef.current.appendChild(colorScaleSvg.node());
     }
-  }, [divRef, svg]);
+  }, [divRef, svg, colorScaleSvg]);
 
   // Return the div element as a React component
   return <div ref={divRef} />;
 };
 
 export default Heatmap;
+  
