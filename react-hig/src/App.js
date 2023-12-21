bda22dd4d586b18192f5f73371079af92d0f1425
John Nabil Hanna <Johnnabil67@gmail.com>
2023-12-21 14:14:24 +0100

    typo

--- react-hig/src/App.js
+++ react-hig/src/App.js
@@ -10,7 +10,7 @@ function App() {
   };
 
   return (
-    <div class="heatmap-container">
+    <div className="heatmap-container">
       <SortingWidgets
         id="widget"
         inputText={inputText}
