ceb01e3b4f3bcb40a824e9357d66a620e3a40c17
willberj <wib@live.se>
2024-01-02 10:10:51 +0100

    test

--- react-hig/src/BackgroundVideo.js
+++ react-hig/src/BackgroundVideo.js
@@ -3,7 +3,7 @@ import "./BackgroundVideo.css";
 
 const BackgroundVideo = ({ videoId }) => {
   const [muted, setMuted] = useState(true);
-  const videoSrc = `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=2&controls=0&loop=1&playlist=${videoId}&modestbranding=1&showinfo=0&rel=0&iv_load_policy=3`;
+  const videoSrc = `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&controls=0&loop=1&playlist=${videoId}&modestbranding=1&showinfo=0&rel=0&iv_load_policy=3`;
 
   return (
     <div className="video-background">
