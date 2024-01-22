bcfce344c0292c1ee8367e8c1359efc8a301e9a8
willberj <wib@live.se>
2024-01-22 23:18:02 +0100

    Fixade så man inte kan skriva vad som helst i urlen.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import "./App.css";
 import Heatmap from "./Heatmap";
 import SortingWidgets from "./SortingWidgets";
 import { COLORS } from "./values/colors";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   const [inputText, setInputText] = useState("");
@@ -17,17 +23,27 @@ function App() {
   }, []);
 
   return (
-    <div
-      className="heatmap-container"
-      style={{ backgroundColor: COLORS.background }}
-    >
-      <SortingWidgets
-        id="widget"
-        inputText={inputText}
-        onInputChange={handleTextChange}
-      />
-      <Heatmap inputText={inputText} />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div
+              className="heatmap-container"
+              style={{ backgroundColor: COLORS.background }}
+            >
+              <SortingWidgets
+                id="widget"
+                inputText={inputText}
+                onInputChange={handleTextChange}
+              />
+              <Heatmap inputText={inputText} />
+            </div>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
